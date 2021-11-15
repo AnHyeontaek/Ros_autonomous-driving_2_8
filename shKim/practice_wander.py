009cbf1c68e346cd1b9ba876885fe8b6c2559342
SeonhyunKim <sun12334567@gmail.com>
2021-11-15 05:05:14 +0000

    made stop at stop line source

--- shKim/practice_wander.py
+++ shKim/practice_wander.py
@@ -11,7 +11,7 @@ def scan_callback(msg):
 
 g_range_ahead = 1 # anything to start
 scan_sub = rospy.Subscriber('scan', LaserScan, scan_callback)
-cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+cmd_vel_pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist, queue_size=1)
 rospy.init_node('wander')
 state_change_time = rospy.Time.now()
 driving_forward = True
@@ -20,16 +20,16 @@ rate = rospy.Rate(10)
 while not rospy.is_shutdown():
     if driving_forward:
     # BEGIN FORWARD
-        if (g_range_ahead < 1.5 ):
+        if (g_range_ahead < 2.0):
             #if sensor value is under 1.5
             driving_forward = False
-            state_change_time = rospy.Time.now() + rospy.Duration(5)
+            state_change_time = rospy.Time.now() + rospy.Duration(3)
             print("Obstacle detect")
 
     else:
         if rospy.Time.now() > state_change_time:
             driving_forward = True
-            state_change_time = rospy.Time.now() + rospy.Duration(7)
+            state_change_time = rospy.Time.now() + rospy.Duration(5)
 
     twist = Twist()
     if driving_forward:
