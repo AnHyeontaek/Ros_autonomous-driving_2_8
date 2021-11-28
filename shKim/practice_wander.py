7aae6f2667a5114c4595b3e39826fdeba22e0697
Seonhuyn Kim <sun12334567@gmail.com>
2021-11-28 03:29:10 +0000

    add change turtlebot's location description

--- shKim/practice_wander.py
+++ shKim/practice_wander.py
@@ -6,8 +6,17 @@ from sensor_msgs.msg import LaserScan
 
 def scan_callback(msg):
     global g_range_ahead
-    g_range_ahead = min(msg.ranges)
+
+    # #g_range_ahead = min(msg.ranges)
+    # g_range_ahead = min(msg.ranges)
+    #g_range_ahead = msg.ranges[(len(msg.ranges) / 2)]
+
+    range_size = len(msg.ranges)
+    g_range_ahead = reduce(lambda x, y: x + y, list(
+         filter(lambda x: x >= 0, msg.ranges[range_size / 2:range_size * 3 / 4])))
     print 'g_range_ahead = %.1f' % g_range_ahead
+
+
 
 g_range_ahead = 1 # anything to start
 scan_sub = rospy.Subscriber('scan', LaserScan, scan_callback)
@@ -18,24 +27,27 @@ driving_forward = True
 rate = rospy.Rate(10)
 
 while not rospy.is_shutdown():
-    if driving_forward:
-    # BEGIN FORWARD
-        if (g_range_ahead < 2.0):
-            #if sensor value is under 1.5
-            driving_forward = False
-            state_change_time = rospy.Time.now() + rospy.Duration(3)
-            print("Obstacle detect")
 
-    else:
-        if rospy.Time.now() > state_change_time:
-            driving_forward = True
-            state_change_time = rospy.Time.now() + rospy.Duration(5)
+    # if driving_forward:
+    # # BEGIN FORWARD
+    #     if (g_range_ahead < 1.0):
+    #         #if sensor value is under 1.5
+    #         # if(g_range_ahead > )
+    #         driving_forward = False
+    #         #state_change_time = rospy.Time.now() + rospy.Duration(3)
+    #         print("Obstacle detect, stop 3s second")
+    #
+    # else:
+    #     if rospy.Time.now() > state_change_time:
+    #         driving_forward = True
+    #         #state_change_time = rospy.Time.now() + rospy.Duration(5)
 
     twist = Twist()
-    if driving_forward:
-        twist.linear.x = 0.5
-        # else:
-        #     twist.angular.z = 1#
-    cmd_vel_pub.publish(twist)
 
+    # if 130 < g_range_ahead < 204:
+    #     twist.linear.x = 0
+    #
+    # else:
+    #     twist.linear.x = 0.7
+    cmd_vel_pub.publish(twist)
     rate.sleep()
