38a792d68cbc4ec22a745208bfc5b3e872a91729
jchyeok <49120273+JeonChanHyeok@users.noreply.github.com>
2021-12-03 16:50:28 +0900

    Update find_obstacle.py

--- deu_car/scripts/find_obstacle.py
+++ deu_car/scripts/find_obstacle.py
@@ -20,18 +20,17 @@ class DetectObtacle:
        # LaserScan의 값들 중 평균값을 터틀봇에 부착된 센서가 정중앙을 바라보는 값으로 한다.
        range_right = max(msg.ranges)
        # LaserScan의 값들 중 최댓값을 터틀봇에 부착된 센서가 오른쪽을 바라보는 값으로 한다.
-       if range_center > 2.0 or range_right > 1.5 or \
-               ((math.isnan(range_center)) and math.isnan(range_right)):
-       # 중간값이 2.0초과이고 오른쪽값이 1.5초과이면 터틀봇이 주행한다.
-       # 터틀봇이 장애물과 충돌 방지를 위해 정지한 후 장애물이 터틀봇과 멀어지기 시작하면 다시 주행한다.
-           self.twist.linear.x = 1.0
-           self.drive_key.key = "no_obstacle"
-       elif range_center < 2.0 or range_right < 1.5:
-       # 중간값이 2.0미만이고 오른쪽값이 1.5미만이면 정지한다.
-           self.twist.linear.x = 0.0
-           self.drive_key.key = "obstacle"
-       self.drive_key.twist = self.twist
-       self.find_obstacle_pub.publish(self.drive_key)
+      
+       if range_center < 2.0 or range_right < 1.5:
+            # 중간값이 2.0미만이거나 오른쪽값이 1.5미만이면 정지한다.
+            self.drive_key.key = "obstacle" # 장애물을 인식했다고 키를 저장한다.
+            self.drive_key.twist = self.twist
+            self.find_obstacle_pub.publish(self.drive_key) # 저장된 키를 전송한다.
+       else: 
+            self.drive_key.key = "no_obstacle" 
+            self.drive_key.twist = self.twist
+            self.find_obstacle_pub.publish(self.drive_key)
+
 rospy.init_node('DetectObtacle')
 wander = DetectObtacle()
 rospy.spin()
